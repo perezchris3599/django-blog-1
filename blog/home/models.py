765c22f3ce768c65b245bd1598fe416126ea59d4
perezchris3599 <kweeziperez712@gmail.com>
2022-08-05 14:12:45 +0300

    blogmodel

--- blog/home/models.py
+++ blog/home/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from flora_editor.fields import FroalaField
+from froala_editor.fields import FroalaField
 
 class BlogModel(models.Model):
     title = models.CharField(max_length=1000)
@@ -9,6 +9,9 @@ class BlogModel(models.Model):
     image = models.ImageField(upload_to='blog')
     created_at = models.DateTimeField(auto_now_add=True)
     upload_to = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
 
 
 
