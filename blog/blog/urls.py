3e65d7aff708ef8fc347ad189892da3f55652295
perezchris3599 <kweeziperez712@gmail.com>
2022-08-05 16:29:35 +0300

    urls

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,7 +1,7 @@
-"""blog URL Configuration
+"""Blog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,19 +15,23 @@ Including another URLconf
 """
 from os import stat
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path , include
 from django.conf.urls.static import static
 from django.conf import settings
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('' , include('home.urls')),
+    path('api/' , include('home.urls_api')),
+    
     path('admin/', admin.site.urls),
     path('froala_editor/',include('froala_editor.urls'))
 ]
 
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
+
 
 urlpatterns += staticfiles_urlpatterns()
